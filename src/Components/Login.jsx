2581c36e3dc43500257c14452706310f5437e447
Siddhant sahu <114759848+sidddsahu@users.noreply.github.com>
2024-03-23 14:19:34 +0530

    last commit

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -9,10 +9,6 @@ function Login({ onLogin }) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if(password !== "pistol"){
-      alert("Password not correct")
-      return;
-    }
     try {
       const response = await axios.post('https://reqres.in/api/login', {
         email,
@@ -21,52 +17,73 @@ function Login({ onLogin }) {
       onLogin(response.data);
       navigate('/'); // Redirect to home on successful login
     } catch (error) {
-      alert(error.response.data.error)
+      console.error(error);
       // Handle login errors (e.g., display error message)
     }
   };
 
   return (
-    <div className='w-full login m-auto flex justify-center align-middle'>
-   <div className=' login-inner  drop-shadow-2xl rounded-lg p-5 bg-slate-300 '>
-      <h1 className='text-3xl  font-semibold'>Login...</h1>
-      <form onSubmit={handleSubmit}>
-        <div className=''>
-
-        <div className='flex flex-col mb-4 mt-4 '>
-        <label className='text-xl font-semibold ' htmlFor="email">Email:</label>
-        <input
-        className='p-1  rounded'
-        placeholder='Enter your email...'
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
+    <div className="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
+      <div
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
         />
+      </div>
+      <div className="mx-auto max-w-2xl text-center">
+        <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Login</h2>
+        <p className="mt-2 text-lg leading-8 text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste distinctio nemo cumque.
+        </p>
+      </div>
+      <form onSubmit={handleSubmit} className="mx-auto mt-16 max-w-xl sm:mt-20">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+          {/*  */}
+          <div>
+            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+            <div className="mt-2.5">
+              <input
+                className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                placeholder='Enter your email...'
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+          </div>
+          {/*  */}
+          <div>
+            <label htmlFor="password" className="block text-sm font-semibold leading-6 text-gray-900">
+              Password
+            </label>
+            <div className="mt-2.5">
+              <input
+                placeholder='*************'
+                type="password"
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <button className='block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' type="submit">
+              Login
+            </button>
+          </div>
         </div>
-
-        <div className='flex flex-col  text-xl font-semibold'>
-        <label htmlFor="password">Password:</label>
-        <input className='p-1 rounded'
-        placeholder='*************'
-          type="password"
-          id="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        </div>
-
-        </div>
-
-
-
-        <button className='border px-12 py-2 mt-5 hover:bg-pink-500 font-semibold bg-black text-white' type="submit">Login</button>
       </form>
     </div>
-    </div>
-
   );
 }
 
